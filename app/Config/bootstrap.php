66fe01428b6a459bd53b5276a07dc6d5e452eb3e
Ondřej Cífka <ondra@cifka.com>
2014-08-01 14:44:14 +0200

    Add bootstrap.local.php

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -22,6 +22,8 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+include "bootstrap.local.php"; // Sets timezone etc.
+
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 
