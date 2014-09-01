be7e6ee138bd31787b57025e6af673a652694415
Ondřej Cífka <ondra@cifka.com>
2014-09-01 13:10:24 +0200

    Redirect /admin to login

--- src/app/Config/routes.php
+++ src/app/Config/routes.php
@@ -30,6 +30,12 @@
 	Router::connect('/translators/settings/*', array('controller' => 'translators', 'action' => 'edit_settings'));
 
 /**
+ * Redirects.
+ */
+
+  Router::redirect('/admin', array('controller' => 'admins', 'action' => 'login', 'admin' => 'true'));
+
+/**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
