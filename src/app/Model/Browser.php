dbd238c8b9d1d6ddaa40cd3b0280184c868bb9d3
Ondřej Cífka <ondra@cifka.com>
2014-10-27 17:52:53 +0100

    Add browser identification to translations

---
+++ src/app/Model/Browser.php
@@ -0,0 +1,28 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class Browser extends AppModel {
+  public $actsAs = array('Containable');
+
+  public function current(){
+    $hash = $this->getCurrentHash();
+    if(is_null($hash)) return null;
+
+    $browser = $this->findByHash($hash);
+    if(!$browser){
+      $this->create(array(
+        'hash' => $hash,
+        'user_agent' => env('HTTP_USER_AGENT'),
+        'ip_address' => env('REMOTE_ADDR')
+      ));
+      $browser = $this->save();
+    }
+  
+    return $browser;
+  }
+
+  public function getCurrentHash(){
+    return md5(env('HTTP_USER_AGENT') . env('REMOTE_ADDR'));
+  }
+
+}
