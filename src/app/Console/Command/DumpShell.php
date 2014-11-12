2bef8deb8f4dcfdf79bc3e1418cef6bff44dbf24
Ondřej Cífka <ondra@cifka.com>
2014-11-12 09:46:05 +0000

    Add a command for a daily dump of translations

--- src/app/Console/Command/DumpShell.php
+++ src/app/Console/Command/DumpShell.php
@@ -49,5 +49,41 @@ class DumpShell extends AppShell {
     }
   }
 
+  public function translations(){
+    $data = $this->Translation->find('all', array(
+      'contain' => array('Post' => array('Lang'), 'Lang'),
+      'order' => 'Translation.created ASC'
+    ));
+
+    $this->dumpTranslations($data);
+  }
+
+  public function dailyTranslations(){
+    $data = $this->Translation->find('all', array(
+      'contain' => array('Post' => array('Lang'), 'Lang'),
+      'order' => 'Translation.created ASC',
+      'conditions' => array('Translation.created > NOW() - INTERVAL 24 HOUR')
+    ));
+
+    $this->dumpTranslations($data);
+  }
+
+  private function dumpTranslations($data){
+    $mt = $this->Translator->findByEmail(Configure::read('MT.Translator.email'));
+    foreach($data as $d){
+      $tr = $d['Translation'];
+      if($tr['translator_id'] != $mt['Translator']['id']){
+        $this->out(
+          $d['Post']['id'] ."\t".
+          $d['Post']['Lang']['code'] ."\t".
+          preg_replace('/\s+/', ' ', $d['Post']['text']) ."\t".
+          $tr['id'] ."\t".
+          $d['Lang']['code'] ."\t".
+          preg_replace('/\s+/', ' ', $tr['text']) ."\t"
+        );
+      }
+    }
+  }
+
 
 }
